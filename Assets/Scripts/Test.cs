d303fb896d2beb8badd5df5fb754697fcee23944
Embogram <kkreyca@gmail.com>
2021-09-15 12:59:06 +0200

    print

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Console.Write("Hello Wolrd");
+        print("afafasffa");
     }
 
     // Update is called once per frame
