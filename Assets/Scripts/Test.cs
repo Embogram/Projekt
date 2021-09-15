afdb3a9bb348077ba6d172b4fcfd4e659aca8c42
Mirzet <mtabakovic22@gmail.com>
2021-09-15 13:08:42 +0200

    Changes in Script Test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,14 +8,16 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
- print("afafasffa");
+        int x = 1 + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetX();
     }
-
+    private int GetX()
+    {
+        return 1;
+    }
 }
